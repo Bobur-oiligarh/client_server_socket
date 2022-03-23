59914b2c4e74f8e023c740c61d131c4308f1f85f
Bobur-oiligarh <uboiligarh@gmail.com>
2022-03-23 17:10:09 +0500

    Client, Server classes are realized without login/register fungit

--- client.py
+++ client.py
@@ -1,27 +1,49 @@
 import socket
-
-HEADER = 64
-PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.64.1"
-ADDR = (SERVER, PORT)
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)   # It is header message.
-    msg_header = str(msg_length).encode(FORMAT)  # We have changed it format to be able to send the server.
-    msg_header += b' ' * (HEADER - len(msg_header))  # We fill empty bytes to have 64 bytes size for HEADER in the end.
-    client.send(msg_header)
-    client.send(message)
+import threading
 
 
-send("Hello World!")
-send("Hello everybody")
-send("Hello Bob")
-send("Hello Madish")
+class Client:
+    """ """
 
-send(DISCONNECT_MESSAGE)
+    def __init__(self, server_host, port):
+        self.server_host = server_host
+        self.port = port
+        self.nickname = input("Choose a nickname:  ")
+
+    def connect_to_server(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((server_host, port))
+
+    def receive_messages(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.client_socket.send(self.nickname.encode('utf-8'))
+
+                else:
+                    print(message)
+            except:
+                print('An error occurred!')
+                self.client_socket.close()
+                break
+
+    def send_message(self):
+        while True:
+            message = f'{self.nickname}: {input("")}'
+            self.client_socket.send(message.encode('utf-8'))
+
+    def thread_handle(self):
+        receive_message_thread = threading.Thread(target=self.receive_messages)
+        receive_message_thread.start()
+
+        send_message_thread = threading.Thread(target=self.send_message)
+        send_message_thread.start()
+
+
+if __name__ == '__main__':
+    server_host = socket.gethostbyname(socket.gethostname())
+    port = 50505
+    client = Client(server_host, port)
+    client.connect_to_server()
+    client.thread_handle()
--- server.py
+++ server.py
@@ -1,40 +1,65 @@
 import socket
 import threading
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+class Server:
+    """ """
+    def __init__(self, server_host, port):
+        self.server_host = server_host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = []
+        self.clients_nicknames = []
+
+    def create_server(self):
+        self.server_socket.bind((self.server_host, self.port))
+        self.server_socket.listen()
+
+    def send_broadcast_message(self, message):
+        for client in self.clients:
+            client.send(message)
+
+    def message_handle(self, client):
+        while True:
+            try:
+                message = client.recv(1024)
+                self.send_broadcast_message(message)
+            except:
+                index = self.clients.index(client)
+                self.clients.remove(client)
+                client.close()
+                client_nickname = self.clients_nicknames[index]
+                self.send_broadcast_message(f"{client_nickname} left the chat".encode('utf-8'))
+                self.clients_nicknames.remove(client_nickname)
+                break
+
+    def receive_message(self):
+        while True:
+            client, address = self.server_socket.accept()
+            print(f"Connected with {str(address)}")
+
+            client.send('NICK'.encode('utf-8'))
+            client_nickname = client.recv(1024).decode('utf-8')
+            self.clients_nicknames.append(client_nickname)
+            self.clients.append(client)
+
+            print(f"Nickname of the client is  - {client_nickname}")
+            self.send_broadcast_message(f"{client_nickname} joined the chat!".encode('utf-8'))
+            client.send('You have connected to the server!'.encode('utf-8'))
+
+            thread = threading.Thread(target=self.message_handle, args=(client,))
+            thread.start()
+
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1} ")
+            print(self.clients_nicknames)
+
+if __name__ == '__main__':
+    server_host = socket.gethostbyname(socket.gethostname())
+    port = 50505
+    server = Server(server_host, port)
+    server.create_server()
+    print("[STARTING] server is starting ...")
+    server.receive_message()
 
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
 
-    connected = True
-    while True:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-            print(f"[{addr}] {msg}")
-    conn.close()
-
-
-def start():
-    server.listen()
-    print(f"[LISTENING] SERVER is listening on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1} ")
-
-print("[STARTING] server is starting ...")
-start()
