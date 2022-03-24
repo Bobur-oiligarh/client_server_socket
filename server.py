a74396ac3de11fe35ffb02d84f6128d2fb121e9a
Bobur-oiligarh <uboiligarh@gmail.com>
2022-03-24 07:38:31 +0500

    login/register methods added

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import json
 import socket
 import threading
 
@@ -20,20 +21,41 @@ class Server:
             if sender != client:
                 client.send(message)
 
-    def login(self, client):
-        while True:
-            try:
-                message = client.recv(1024).decode("utf-8")
-                if str(message).lower() == "login":
+    def user_register(self, client):
+        client.send(f"Please type your username to register to the service: ".encode('utf-8'))
+        username = client.recv(1024).decode('utf-8')
+        client.send(f"Please type password to end the register to the service: ".encode('utf-8'))
+        password = client.recv(1024).decode('utf-8')
+        new_user ={"username": username, "password": password}
 
-            except:
-                pass
+        with open('users_datas.json', 'r') as f:
+            allowed_users = json.load(f)
+            allowed_users.append(new_user)
+        with open('users_datas.json', 'w') as f:
+            json.dump(allowed_users, f)
+
+    def user_log_in(self, client):
+        client.send(f"Please type your username to log in the service: ".encode('utf-8'))
+        username = client.recv(1024).decode('utf-8')
+
+        with open('users_datas.json', 'r') as f:
+            allowed_users = json.load(f)
+            for data in allowed_users:
+                if username in data["username"]:
+                    client.send(f"Please type password to end the log in the service: ".encode('utf-8'))
+                    password = client.recv(1024).decode('utf-8')
+                    if password == data["password"]:
+                        client.send(f"You have passed log in successfully, enjoy our chat".encode('utf-8'))
+                else:
+                    client.send(f"[{client.data[1]}]you should register before log in. "
+                                f"Please type 'register' to be able registering".encode('utf-8'))
+                    self.register()
 
     def message_handle(self, client):
         while True:
             try:
                 message = client.recv(1024)
-                self.send_broadcast_message(client, f"[{message}]")
+                self.send_broadcast_message(client, message)
             except:
                 index = self.clients.index(client)
                 self.clients.remove(client)
@@ -47,15 +69,21 @@ class Server:
         while True:
             client, address = self.server_socket.accept()
             print(f"User connected with address - {str(address)}")
-
             client.send('NICK'.encode('utf-8'))
             client_nickname = client.recv(1024).decode('utf-8')
             self.clients_nicknames.append(client_nickname)
             self.clients.append(client)
 
             print(f"Nickname of the client is  - [{client_nickname}]")
-            self.send_broadcast_message(client, f"[{client_nickname}] joined the chat!".encode('utf-8'))
-            client.send(f"[{client_nickname}] - you have connected to the server!".encode('utf-8'))
+            self.send_broadcast_message(client, f"{client_nickname} joined the chat!".encode('utf-8'))
+            client.send(f"You have connected to the server!".encode('utf-8'))
+
+            client.send(f"To log in to the chat type - 'login', type 'register' to register the chat".encode('utf-8'))
+            message = client.recv(1024).decode('utf-8')
+            if message.lower() == "login":
+                self.user_log_in(client)
+            elif message.lower() == "register":
+                self.user_register(client)
 
             thread = threading.Thread(target=self.message_handle, args=(client,))
             thread.start()
